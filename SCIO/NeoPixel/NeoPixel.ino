2c6780d8753592ed29435f309a70bd2b6a534f84
Maximilian Johannes Wurmitzer <johannes@wurmitzer.at>
2019-09-15 20:13:57 +0200

    Signed-off-by: Maximilian Johannes Wurmitzer <johannes@wurmitzer.at>

--- SCIO/NeoPixel/NeoPixel.ino
+++ SCIO/NeoPixel/NeoPixel.ino
@@ -1,9 +1,12 @@
+// https://www.arduino.cc/reference/en/
+
+
 #include <Adafruit_NeoPixel.h>
 
 // https://funduino.de/nr-17-ws2812-neopixel
 
 #define PIN 9 // Hier wird angegeben, an welchem digitalen Pin die WS2812 LEDs bzw. NeoPixel angeschlossen sind
-#define NUMPIXELS 12 // Hier wird die Anzahl der angeschlossenen WS2812 LEDs bzw. NeoPixel angegeben
+#define NUMPIXELS 256 // Hier wird die Anzahl der angeschlossenen WS2812 LEDs bzw. NeoPixel angegeben
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 int pause=100; // 100 Millisekunden Pause bis zur Ansteuerung der nächsten LED.
@@ -15,10 +18,20 @@ void setup() {
   Serial1.begin(115200); // Initialisierung der seriellen Schnittstelle
 }
 
+byte color;
+byte idx;
+
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.write("Hello World 0\n");
   Serial1.write("Hello World 1\n");
+  for (idx = 0; idx < 12; idx++){
+    pixels.setPixelColor(idx, pixels.Color((color+idx) % 25, (color+idx+8) % 25, (color+idx+16) % 25)); // Pixel1 leuchtet in der Farbe Grün
+  }
+  color++;
+  if (color >= 250) color = 0;
+  delay(25);
+/*
   pixels.setPixelColor(0, pixels.Color(25, 0,0)); // Pixel1 leuchtet in der Farbe Grün
   pixels.setPixelColor(1, pixels.Color(25, 0,0)); // Pixel1 leuchtet in der Farbe Grün
   pixels.setPixelColor(2, pixels.Color(25, 0,0)); // Pixel1 leuchtet in der Farbe Grün
@@ -31,7 +44,7 @@ void loop() {
   pixels.setPixelColor(9, pixels.Color(0, 0,25)); // Pixel1 leuchtet in der Farbe Grün
   pixels.setPixelColor(10, pixels.Color(0, 0,25)); // Pixel1 leuchtet in der Farbe Grün
   pixels.setPixelColor(11, pixels.Color(0, 0,25)); // Pixel1 leuchtet in der Farbe Grün
-  
+*/  
 /*  
 pixels.setPixelColor(1, pixels.Color(0,255,0)); // Pixel1 leuchtet in der Farbe Grün
 pixels.show(); // Durchführen der Pixel-Ansteuerung
@@ -87,6 +100,7 @@ pixels.setPixelColor(9, pixels.Color(0,0,0));
 pixels.setPixelColor(10, pixels.Color(0,0,0)); 
 pixels.setPixelColor(11, pixels.Color(0,0,0)); */
 pixels.show(); // Durchführen der Pixel-Ansteuerung
+/*
 delay (pause); // Pause, die LEDs bleiben in dieser Zeit aus
-
+*/
 }
