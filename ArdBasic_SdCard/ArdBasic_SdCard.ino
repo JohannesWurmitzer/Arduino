32f6132eb2d3d53498ef03fb5db52f1335c04bd6
JohannesWurmitzer <johannes.wurmitzer@gmail.com>
2021-04-13 21:09:19 +0200

    ArdBasic_SdCard - improvements
    improvements to read also corrupted directory entries

--- ArdBasic_SdCard/ArdBasic_SdCard.ino
+++ ArdBasic_SdCard/ArdBasic_SdCard.ino
@@ -23,6 +23,12 @@
 #include <SPI.h>
 #include <SD.h>
 
+
+#define USE_TEST_CARD      //
+//#define USE_TEST_CARD_SD      //
+//#define USE_PRINT_DIRECTORY   //
+//#define SD_ERROR_READ_IMPLEMENTED //
+
 #define PIN_MISO  50
 #define PIN_MOSI  51
 #define PIN_SSPK  52
@@ -35,6 +41,10 @@
 // MKRZero SD: SDCARD_SS_PIN
 //const int chipSelect = 4;
 const int chipSelect = 53;
+
+#ifdef USE_PRINT_DIRECTORY
+File root;
+#endif
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -64,14 +74,22 @@ void setup() {
   delay(50);
 //  SPI.begin();
  while(1){
-//  testCard();
+#ifdef USE_TEST_CARD
+  testCard();
+#endif
+#ifdef USE_TEST_CARD_SD
   testCard_SD();
+#endif
+#ifdef USE_PRINT_DIRECTORY
+  testCard_SD();
+#endif
   SPI.end();
   digitalWrite(PIN_MISO, LOW);
   pinMode(PIN_MISO, OUTPUT);
   delay(1000);
  }
 }
+
 
 void testCard_SD(void){
   Serial.print("Initializing SD card...");
@@ -80,7 +98,11 @@ void testCard_SD(void){
 //  SD.end();
 //  if (!SD.begin(chipSelect)) {
   if (!SD.begin(SPI_HALF_SPEED, chipSelect)) {
+#ifdef SD_ERROR_READ_IMPLEMENTED
     Serial.print("Card failed, or not present"); Serial.print("Err-#: "); Serial.print(SD.error()); Serial.println();
+#else
+    Serial.print("Card failed, or not present"); /*Serial.print("Err-#: "); Serial.print(SD.error()); */Serial.println();
+#endif
     // don't do anything more:
 //    digitalWrite(chipSelect, LOW); // Add this line
 //    delay(50);
@@ -91,10 +113,19 @@ void testCard_SD(void){
   }
   else{
     Serial.println("card initialized.");
+
+#ifdef USE_PRINT_DIRECTORY
+
+    root = SD.open("/");
+  
+    printDirectory(root, 0);
+  
+    Serial.println("done!");
+#endif    
   }
 }
 
-
+#ifdef USE_TEST_CARD
 
 // set up variables using the SD utility library functions:
 Sd2Card card;
@@ -173,7 +204,87 @@ void testCard(){
   // list all files in the card with date and size
   root.ls(LS_R | LS_DATE | LS_SIZE);
   root.close();
+
+  Serial.println("--- ENDE ---");
+  // https://www.ulrichradig.de/forum/viewtopic.php?f=47&t=1981
+  
+  //https://forum.arduino.cc/t/sd-verzeichnis-als-liste-lesen/473526/3
+  //Verzeichnis SD lesen
+  root.openRoot(volume);
+  dir_t p;
+  
+  root.rewind();
+  
+  while (root.readDir(p) > 0) {
+  
+    if (p.name[0] == DIR_NAME_FREE) break;
+  
+    if (p.name[0] == DIR_NAME_DELETED || p.name[0] == '.') continue;
+  
+    if (!DIR_IS_FILE_OR_SUBDIR(&p)) continue;
+  
+    for (uint8_t i = 0; i < 11; i++) 
+    {
+  
+      if (p.name[i] == ' ') continue;
+  
+      if (i == 8) 
+      {
+        Serial.print('.');
+      }  
+  
+      Serial.print((char)p.name[i]);     
+    }
+  
+    Serial.write('\n');// Terminator
+  }
 }
+#endif
+
+#ifdef USE_PRINT_DIRECTORY
+void printDirectory(File dir, int numTabs) {
+
+  while (true) {
+
+    File entry =  dir.openNextFile();
+
+    if (! entry) {
+
+      // no more files
+
+      break;
+
+    }
+
+    for (uint8_t i = 0; i < numTabs; i++) {
+
+      Serial.print('\t');
+
+    }
+
+    Serial.print(entry.name());
+
+    if (entry.isDirectory()) {
+
+      Serial.println("/");
+
+      printDirectory(entry, numTabs + 1);
+
+    } else {
+
+      // files have sizes, directories do not
+
+      Serial.print("\t\t");
+
+      Serial.println(entry.size(), DEC);
+
+    }
+
+    entry.close();
+
+  }
+}
+#endif
 
 void loop(void) {
 }
